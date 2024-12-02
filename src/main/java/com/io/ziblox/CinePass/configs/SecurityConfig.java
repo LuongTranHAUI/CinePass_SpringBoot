559dc4fbc0b97f7485ec6e895751e70fe77f98eb
Trần Đức Lương <76874149+NoName2410@users.noreply.github.com>
2024-12-02 16:40:25 +0700

    Keycloak setup

--- src/main/java/com/io/ziblox/CinePass/configs/SecurityConfig.java
+++ src/main/java/com/io/ziblox/CinePass/configs/SecurityConfig.java
@@ -2,22 +2,31 @@ package com.io.ziblox.CinePass.configs;
 
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @KeycloakConfiguration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
@@ -30,7 +39,22 @@ public class SecurityConfig {
     @Bean
     public JwtAuthenticationConverter jwtAuthenticationConverter() {
         JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
-        // Customize the converter if needed
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+
+            if (resourceAccess != null && resourceAccess.containsKey("keycloak")) {
+                Map<String, Object> keycloakAccess = (Map<String, Object>) resourceAccess.get("keycloak");
+                List<String> roles = (List<String>) keycloakAccess.get("roles");
+
+                if (roles != null) {
+                    return roles.stream()
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                            .collect(Collectors.toList());
+                }
+            }
+
+            return Collections.emptyList();
+        });
         return converter;
     }
 
--- src/main/java/com/io/ziblox/CinePass/controllers/MovieController.java
+++ src/main/java/com/io/ziblox/CinePass/controllers/MovieController.java
@@ -1,8 +1,8 @@
 package com.io.ziblox.CinePass.controllers;
 
-import com.io.ziblox.CinePass.models.dtos.MovieDto;
 import com.io.ziblox.CinePass.exceptions.DataNotFoundException;
 import com.io.ziblox.CinePass.exceptions.InvalidParamException;
+import com.io.ziblox.CinePass.models.dtos.MovieDto;
 import com.io.ziblox.CinePass.models.responses.MovieResponse;
 import com.io.ziblox.CinePass.models.responses.PagedResponse;
 import com.io.ziblox.CinePass.services.MovieService;
@@ -13,6 +13,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
@@ -22,40 +24,42 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("${api.prefix}/movies") //Đánh version cho API giúp nâng cấp dễ dàng hơn
+@RequestMapping("${api.prefix}/movies")
 @RequiredArgsConstructor
+
 public class MovieController {
     private final MovieService movieService;
 
-    //Thêm một phim mới
     @PostMapping(value = "")
+    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<?> createMovie(
-            @Valid
-            @RequestBody MovieDto movieDto,
-            BindingResult bindingResult
+            @Valid @RequestBody MovieDto movieDto,
+            BindingResult bindingResult,
+            Authentication authentication
     ) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .toList());
+        }
         try {
-            if (bindingResult.hasErrors()) {
-                return ResponseEntity.badRequest().body(bindingResult.getFieldErrors()
-                        .stream()
-                        .map(FieldError::getDefaultMessage)
-                        .toList());
-            }
             movieService.createMovie(movieDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Create a new movie successfully");
+            return ResponseEntity.status(HttpStatus.CREATED).body("Create a new movie successfully.");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while processing the request");
         }
     }
 
-    //Thêm ảnh cho phim mới
     @PostMapping(value = "uploads/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<?> uploadImages(
             @PathVariable int id,
             @RequestParam("files") List<MultipartFile> files
     ) {
         try {
             movieService.addImagesToMovie(id, files);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Uploaded images successfully.");
         } catch (DataNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (IOException e) {
@@ -63,11 +67,9 @@ public class MovieController {
         } catch (InvalidParamException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
-        return ResponseEntity.status(HttpStatus.CREATED).body("Uploaded images successfully");
     }
 
-    //Hiển thị danh sách các phim
-    @GetMapping("") //http://localhost:8088/api/v1/movies?page=1&limit=10
+    @GetMapping("")
     public ResponseEntity<?> getMovies(
             @RequestParam("page") int page,
             @RequestParam("limit") int limit
@@ -80,11 +82,10 @@ public class MovieController {
         return ResponseEntity.ok().body(pagedResponse);
     }
 
-    //Hiển thị thông tin chi tiết một phim theo id
-    @GetMapping("/{id}") //http://localhost:8088/api/v1/movies/1
+    @GetMapping("/{id}")
     public ResponseEntity<?> getMovieById(
             @PathVariable("id") int id
-    ) throws DataNotFoundException {
+    ) {
         try {
             MovieResponse movieResponse = movieService.getMovieById(id);
             return ResponseEntity.ok().body(movieResponse);
@@ -95,12 +96,11 @@ public class MovieController {
         }
     }
 
-    //Sửa thông tin một phim
     @PutMapping("/{id}")
+    @PreAuthorize("hasRole('MANAGER')")
     public ResponseEntity<?> updateMovie(
             @PathVariable("id") int id,
-            @Valid
-            @RequestBody MovieDto movieDto,
+            @Valid @RequestBody MovieDto movieDto,
             BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) {
@@ -119,10 +119,9 @@ public class MovieController {
         }
     }
 
-    //Xóa một phim
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteMovie(
-            @Valid
             @PathVariable("id") int id
     ) {
         try {
@@ -136,15 +135,16 @@ public class MovieController {
     }
 
     @PostMapping("/generateFakeData")
-    public ResponseEntity<?> generateFakeData() {
+    @PreAuthorize("hasRole('MANAGER')")
+    public ResponseEntity<?> generateFakeData(Authentication authentication) {
         try {
-            for (int i = 0; i < 10; i++){
+            for (int i = 0; i < 10; i++) {
                 MovieDto fakeMovie = movieService.generateFakeMovie();
                 movieService.createMovie(fakeMovie);
             }
-            return ResponseEntity.ok().body("Generated fake data successfully");
+            return ResponseEntity.ok().body("Generated fake data successfully.");
         } catch (Exception e) {
-            return ResponseEntity.status(400).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
 }
